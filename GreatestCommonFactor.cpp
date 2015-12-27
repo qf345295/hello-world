f4a0e29cd3b91a3c479831a5c37631bc886a66af
qf345295 <frank345295@gmail.com>
2015-12-27 23:55:29 +0800

    made some minor modifications to this file

--- GreatestCommonFactor.cpp
+++ GreatestCommonFactor.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 int gcf(int a, int b)
 {
-	a = a>b?a:b;
-	b = a<b?a:b;
 	int temp;
+	if(a<b)
+	{
+		temp=a;
+		a=b;
+		b=temp;
+	}
+	
 	while(a%b)
 	{
 		temp=a;
@@ -12,4 +17,13 @@ int gcf(int a, int b)
 		b=temp%b;
 	}
 	return b;
+}
+
+int main()
+{
+	int a,b;
+	cout<<"input two integers: ";
+	cin>>a>>b;
+	cout<<"the greatest common factor of "<<a<<','<<b<<" is "<<gcf(a,b)<<endl;
+	return 0;
 }
